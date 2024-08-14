7ac2083ee2a7852332f781fb8482454c880e7191
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-08-14 13:14:20 +0700

    feat: setup tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto"/>
+      <Link href="/profile" style={{color: "blue"}}>
+        Go to Profile
+      </Link>
+    </View>
+  )
+}
+
+export default App;
