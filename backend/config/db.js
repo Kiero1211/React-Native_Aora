5ca15aaddd83d523d42fac211a3565e90188d058
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-08-16 13:29:22 +0700

    feat: setup backend

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.log(`[Error] ${error.message}`);
+        process.exit(1);
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import path from "path";
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+
+// Utils
+import connectDB from "./config/db.js"
+import routes from "./routes/index.js";
+
+// Initialize
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")))
+
+connectDB();
+
+// Config middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+// Pre config every cookie
+app.use(function (req, res, next) {
+    const oldCookie = res.cookie;
+    res.cookie = function (name, value, options) {
+        return oldCookie.call(this, name, value, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            ...options // merge with any options provided when 'cookie' was called
+        });
+    }
+    next();
+});
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
